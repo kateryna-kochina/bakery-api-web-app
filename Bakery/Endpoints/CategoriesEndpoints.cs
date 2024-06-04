ef590203e983f8202fcbd999cd744a97ca0bc1e1
kateryna.kochina <kochina.ev@gmail.com>
2024-06-03 21:04:05 -0400

    Refactor categories endpoint for unit tests compatibility

--- Bakery/Endpoints/CategoriesEndpoints.cs
+++ Bakery/Endpoints/CategoriesEndpoints.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Bakery.Dtos;
 using Bakery.Repositories.Contracts;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace Bakery.Endpoints;
 
@@ -10,83 +11,98 @@ public static class CategoriesEndpoints
     {
         var group = app.MapGroup("categories").WithTags("Categories");
 
-        // GET /categories
-        group.MapGet("/", async (ICategoryRepository categoryRepository, IMapper mapper) =>
-        {
-            var categories = await categoryRepository.GetCategoriesAsync();
-
-            var categoriesDtos = mapper.Map<List<CategoryDetailsDto>>(categories);
-
-            return Results.Ok(categoriesDtos);
-        })
-        .WithName("GetCategories")
+        group.MapGet("/", GetCategoriesAsync)
+        .WithName(nameof(GetCategoriesAsync))
         .Produces<List<CategoryDetailsDto>>(StatusCodes.Status200OK);
 
-        // GET /categories/{id}
-        group.MapGet("/{id}", async (int id, ICategoryRepository categoryRepository, IMapper mapper) =>
-        {
-            var category = await categoryRepository.GetCategoryByIdAsync(id);
-
-            if (category is null)
-            {
-                return Results.NotFound();
-            }
-
-            var categoryDetailsDto = mapper.Map<CategoryDetailsDto>(category);
-
-            return Results.Ok(categoryDetailsDto);
-        })
-        .WithName("GetCategoryById")
+        group.MapGet("/{id}", GetCategoryByIdAsync)
+        .WithName(nameof(GetCategoryByIdAsync))
         .Produces<CategoryDetailsDto>(StatusCodes.Status200OK)
         .Produces(StatusCodes.Status404NotFound);
 
-        // POST /categories
-        group.MapPost("/", async (CreateCategoryDto newCategory, ICategoryRepository categoryRepository, IMapper mapper) =>
-        {
-            var createdCategory = await categoryRepository.CreateCategoryAsync(newCategory);
-
-            var createdCategoryDetailsDto = mapper.Map<CategoryDetailsDto>(createdCategory);
-
-            return Results.Created(
-                $"/categories/{createdCategory!.Id}",
-                createdCategoryDetailsDto);
-        })
-        .WithName("CreateCategory")
+        group.MapPost("/", CreateCategoryAsync)
+        .WithName(nameof(CreateCategoryAsync))
         .Produces<CategoryDetailsDto>(StatusCodes.Status201Created)
         .Produces(StatusCodes.Status400BadRequest);
 
-        // PUT /categories/{id}
-        group.MapPut("/{id}", async (int id, UpdateCategoryDto updatedCategory, ICategoryRepository categoryRepository) =>
-        {
-            var result = await categoryRepository.UpdateCategoryAsync(id, updatedCategory);
-
-            if (!result)
-            {
-                return Results.NotFound("Invalid data provided.");
-            }
-
-            return Results.NoContent();
-        })
-        .WithName("UpdateCategory")
+        group.MapPut("/{id}", UpdateCategoryAsync)
+        .WithName(nameof(UpdateCategoryAsync))
         .Produces(StatusCodes.Status204NoContent)
         .Produces(StatusCodes.Status404NotFound);
 
         // DELETE /categories/{id}
-        group.MapDelete("/{id}", async (int id, ICategoryRepository categoryRepository) =>
-        {
-            var result = await categoryRepository.DeleteCategoryAsync(id);
-
-            if (!result)
-            {
-                return Results.NotFound();
-            }
-
-            return Results.NoContent();
-        })
-        .WithName("DeleteCategory")
+        group.MapDelete("/{id}", DeleteCategoryAsync)
+        .WithName(nameof(DeleteCategoryAsync))
         .Produces(StatusCodes.Status204NoContent)
         .Produces(StatusCodes.Status404NotFound);
 
         return group;
+    }
+
+    // GET /categories
+    public static async Task<Ok<List<CategoryDetailsDto>>> GetCategoriesAsync(ICategoryRepository categoryRepository, IMapper mapper)
+    {
+        var categories = await categoryRepository.GetCategoriesAsync();
+
+        var categoriesDtos = mapper.Map<List<CategoryDetailsDto>>(categories);
+
+        return TypedResults.Ok(categoriesDtos);
+    }
+
+    // GET /categories/{id}
+    public static async Task<Results<Ok<CategoryDetailsDto>, NotFound<string>>> GetCategoryByIdAsync(int id, ICategoryRepository categoryRepository, IMapper mapper)
+    {
+        var category = await categoryRepository.GetCategoryByIdAsync(id);
+
+        if (category is null)
+        {
+            return TypedResults.NotFound(EndpointsConstants.Category.NOT_FOUND_MESSAGE);
+        }
+
+        var categoryDetailsDto = mapper.Map<CategoryDetailsDto>(category);
+
+        return TypedResults.Ok(categoryDetailsDto);
+    }
+
+    // POST /categories
+    public static async Task<Results<Created<CategoryDetailsDto>, BadRequest>> CreateCategoryAsync(CreateCategoryDto newCategory, ICategoryRepository categoryRepository, IMapper mapper)
+    {
+        // TODO: add DTO model validation
+        if (newCategory is null)
+        {
+            return TypedResults.BadRequest();
+        }
+
+        var createdCategory = await categoryRepository.CreateCategoryAsync(newCategory);
+
+        var createdCategoryDetailsDto = mapper.Map<CategoryDetailsDto>(createdCategory);
+
+        return TypedResults.Created($"/categories/{createdCategory!.Id}", createdCategoryDetailsDto);
+    }
+
+    // PUT /categories/{id}
+    public static async Task<Results<NoContent, NotFound<string>>> UpdateCategoryAsync(int id, UpdateCategoryDto updatedCategory, ICategoryRepository categoryRepository)
+    {
+        var result = await categoryRepository.UpdateCategoryAsync(id, updatedCategory);
+
+        if (!result)
+        {
+            return TypedResults.NotFound(EndpointsConstants.Category.NOT_FOUND_MESSAGE);
+        }
+
+        return TypedResults.NoContent();
+    }
+
+    // DELETE /categories/{id}
+    public static async Task<Results<NoContent, NotFound<string>>> DeleteCategoryAsync(int id, ICategoryRepository categoryRepository)
+    {
+        var result = await categoryRepository.DeleteCategoryAsync(id);
+
+        if (!result)
+        {
+            return TypedResults.NotFound(EndpointsConstants.Category.NOT_FOUND_MESSAGE);
+        }
+
+        return TypedResults.NoContent();
     }
 }
---
+++ Bakery/Endpoints/EndpointsConstants.cs
@@ -0,0 +1,19 @@
+namespace Bakery.Endpoints;
+
+public static class EndpointsConstants
+{
+    public static class Category
+    {
+        public const string NOT_FOUND_MESSAGE = "Category not found.";
+    }
+
+    public static class Option
+    {
+        public const string NOT_FOUND_MESSAGE = "Option not found.";
+    }
+
+    public static class Product
+    {
+        public const string NOT_FOUND_MESSAGE = "Product not found.";
+    }
+}
